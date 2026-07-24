5e09d37ea8ec3577e37b324bfddfd026fc610829
Esteban Zárate <estebanzarateok@google.com>
2026-07-24 15:26:35 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,14 +36,6 @@ export default defineConfig({
 
     },
     {
-      name: "Victor Mono",
-      cssVariable: "--font-victor",
-      provider: fontProviders.google(),
-      weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      styles: ["normal", "italic"],
-
-    },
-    {
       name: "Jost",
       cssVariable: "--font-jost",
       provider: fontProviders.google(),
