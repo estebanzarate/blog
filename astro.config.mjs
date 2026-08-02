d2656996df715862ab46fd4efefc2c2a41bf3c5e
Esteban Zárate <estebanzarateok@google.com>
2026-08-02 04:13:11 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://no0funny.vercel.app',
   integrations: [mdx()],
   adapter: vercel({
     imageService: true,
